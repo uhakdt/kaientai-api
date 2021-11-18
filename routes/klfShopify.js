3b3a0fb2c0fe31de4a025fd0c77f79e54a820488
uhakdt <uhakdt@gmail.com>
2021-11-18 22:15:42 +0000

    klf Shopify now is all fine with order updates.

--- routes/klfShopify.js
+++ routes/klfShopify.js
@@ -1,10 +1,20 @@
-const express = require('express');
-const db = require('../db');
-const request = require('request');
-const email = require('../auxillary/email');
-const GetDateAndTimeNow = require('../auxillary/dateAndTimeNow');
+import express from 'express';
+import request from 'request';
+import * as email from '../auxillary/email.js';
 
 const router = express.Router();
+
+// Request Options
+import { productGetReqOpts, productUpdateStockReqOpts } from './requestOptions/productReqOpts.js';
+import { postcodeCheckReqOpts } from './requestOptions/postcodeReqOpts.js';
+import { userAddReqOpts } from './requestOptions/userReqOpts.js';
+import { addressAddReqOpts } from './requestOptions/addressReqOpts.js';
+
+import { orderAddReqOpts, orderUpdateReqOpts } from './requestOptions/orderReqOpts.js';
+import { orderCheckExtExistsReqOpts } from './requestOptions/orderReqOpts.js';
+
+import { productCheckIfEnoughStock, productFormatOrderProducts } from '../auxillary/product.js';
+import { klfDataMainDecleration } from '../auxillary/dataDeclaration.js';
 
 
 let currentURL = process.env.URL_PROD;
@@ -13,157 +23,43 @@ if(process.env.NODE_ENV === 'Development') {
   currentURL = process.env.URL_DEV
 }
 
-const reqOptGetProducts = {
-  url: `${currentURL}/api/v1/products/all`,
-  method: 'GET'
-};
+const productGetReqOptsRes = productGetReqOpts()
 
-
+//ORDER CREATE
 router.post('/api/v1/klf/shopify/:supplierID', async (req, res) => {
   try {
-    const dataBody = req.body.data;
-
     // Variable declarations
-    let listOfOrderProducts = []
-    for (let i = 0; i < dataBody.line_items.length; i++) {
-      const e = dataBody.line_items[i];
-      let orderProduct = {
-        title: e.variant_title != "" ? e.title + " - " + e.variant_title : e.title,
-        price: e.price,
-        imageUrl: null,
-        quantity: e.quantity,
-      }
-      listOfOrderProducts.push(orderProduct);
-    }
-    console.log("---------------------------------")
-    console.log("ALL ORDER PRODUCTS: \n", listOfOrderProducts)
-    console.log("---------------------------------")
+    const listOfOrderProducts = productFormatOrderProducts(req.body.data.line_items);
+    let dataMain = klfDataMainDecleration(req.body.data, listOfOrderProducts, req.params.supplierID, "add", null);
 
-    let dataMain = {
-      orderID: null,
-      name: dataBody.customer.first_name + " " + dataBody.customer.last_name,
-      email: dataBody.customer.email,
-      phone: dataBody.customer.phone,
-      address1: dataBody.shipping_address.address1,
-      address2: dataBody.shipping_address.address_2,
-      city: dataBody.shipping_address.city,
-      county: dataBody.shipping_address.province,
-      country: dataBody.shipping_address.country,
-      postcode: dataBody.shipping_address.zip,
-      intUserID: null,
-      extUserID: dataBody.customer.id,
-      orderProducts: listOfOrderProducts,
-      totalAmount: dataBody.total_price,
-      extOrderID: dataBody.id,
-      supplierID: req.params.supplierID,
-      supplierContactName: dataBody.supplierContactName,
-      supplierContactEmail: dataBody.supplierContactEmail,
-      dateAndTime: GetDateAndTimeNow()
-    }
-    
-    const reqOptPostCodeCheck = {
-      url: `${currentURL}/api/v1/postcode/check`,
-      method: 'POST',
-      json: {
-        "supplierID": dataMain.supplierID,
-        "code": dataMain.postcode
-      },
-    };
+    const postcodeCheckReqOptsRes = postcodeCheckReqOpts(dataMain);
+    const addressAddReqOptsRes = addressAddReqOpts(dataMain);
+    const userAddReqOptsRes = userAddReqOpts(dataMain);
+    const productUpdateStockReqOptsRes = productUpdateStockReqOpts(dataMain);
 
-    const reqOptAddAddress = {
-      url: `${currentURL}/api/v1/address`,
-      method: 'POST',
-      json: {
-        "address1": dataMain.address1,
-        "address2": dataMain.address2,
-        "city": dataMain.city,
-        "county": dataMain.county,
-        "country": dataMain.country,
-        "postcode": dataMain.postcode
-      },
-    };
-
-    const reqOptAddUser = {
-      url: `${currentURL}/api/v1/user`,
-      method: 'POST',
-      json: {
-        "name": dataMain.name,
-        "email": dataMain.email,
-        "phone": dataMain.phone,
-        "addressID": null,
-        "dateAndTimeSignUp": dataMain.dateAndTime,
-        "profileImageUrl": null,
-        "extUserID": dataMain.extUserID
-      },
-    };
-
-    const reqOptCheckExtOrderExists = {
-      url: `${currentURL}/api/v1/order/ext/${dataMain.extOrderID}`,
-      method: 'GET'
-    }
-
-    const reqOptAddOrder = {
-      url: `${currentURL}/api/v1/order`,
-      method: 'POST',
-      json: {
-        "dateAndTime": dataMain.dateAndTime,
-        "statusID": 1,
-        "supplierID": dataMain.supplierID,
-        "userID": null,
-        "totalAmount": dataMain.totalAmount,
-        "contactName": dataMain.name,
-        "contactEmail": dataMain.email,
-        "contactPhone": dataMain.phone,
-        "address1": dataMain.address1,
-        "address2": dataMain.address2,
-        "city": dataMain.city,
-        "county": dataMain.county,
-        "country": dataMain.country,
-        "postcode": dataMain.postcode,
-        "offerID": null,
-        "extOrderID": dataMain.extOrderID,
-        "cartProducts": dataMain.orderProducts
-      },
-    };
-
-    const reqOptAddOrderProduct = {
-      url: `${currentURL}/api/v1/orderProduct`,
-      method: 'POST',
-      json: {
-        "orderID": null,
-        "title": null,
-        "quantity": null
-      },
-    };
-
-    const reqOptUpdateStock = {
-      url: `${currentURL}/api/v1/product/stock`,
-      method: 'PUT',
-      json: {
-        "id": null,
-        "stock": null
-      },
-    };
+    const orderAddReqOptsRes = orderAddReqOpts(dataMain);
+    const orderCheckExtExistsReqOptsRes = orderCheckExtExistsReqOpts(dataMain);
 
     // CHECK POSTCODE
-    request(reqOptPostCodeCheck, (error, resPostCodeCheck, body) => {
+    request(postcodeCheckReqOptsRes, (error, resPostCodeCheck, body) => {
       if (error) {
         console.log(error);
       } else if (resPostCodeCheck.body.local === 'yes') {
         
         // ADD ADDRESS
-        request(reqOptAddAddress, (error, resAddAddress, body) => {
+        request(addressAddReqOptsRes, (error, resAddAddress, body) => {
           if(error){
             console.log(error);
           } else if (resAddAddress.statusCode === 201) {
             let addressID = resAddAddress.body.data.address.id;
-            reqOptAddUser.json.addressID = addressID;
+            userAddReqOptsRes.json.addressID = addressID;
+
             // ADD USER
-            request(reqOptAddUser, (error, resAddUser, body) => {
-              reqOptAddOrder.json.userID = resAddUser.body.data.user.id;
+            request(userAddReqOptsRes, (error, resAddUser, body) => {
+              orderAddReqOptsRes.json.userID = resAddUser.body.data.user.id;
               dataMain.intUserID = resAddUser.body.data.user.id;
-              if(reqOptAddOrder.json.userID != null && dataMain.intUserID != null){
-                CheckOrderFulfilment(reqOptCheckExtOrderExists, reqOptAddOrder, reqOptAddOrderProduct, reqOptUpdateStock, dataMain, res);
+              if(orderAddReqOptsRes.json.userID != null && dataMain.intUserID != null){
+                CheckOrderFulfilment(orderCheckExtExistsReqOptsRes, orderAddReqOptsRes, productUpdateStockReqOptsRes, dataMain, res);
               }
             })
           }
@@ -194,52 +90,38 @@ router.post('/api/v1/klf/shopify/:supplierID', async (req, res) => {
   }
 })
 
-let CheckOrderFulfilment = async (reqOptCheckExtOrderExists, reqOptAddOrder, reqOptAddOrderProduct, reqOptUpdateStock, dataMain, res) =>  {
-  // CHECK IF ORDER CAN BE FULFILED
-  let orderToBeFulfilledCount = 0;
-  let orderToBeFulfilled = false;
-  request(reqOptGetProducts, (error, resGetProducts, body) => {
-    let listOfProduct = JSON.parse(body);
+let CheckOrderFulfilment = async (orderCheckExtExistsReqOptsRes, orderAddReqOptsRes, productUpdateStockReqOptsRes, dataMain, res) =>  {
+  // GET List of Products
+  request(productGetReqOptsRes, (error, resGetProducts, body) => {
+    let listOfProduct = JSON.parse(resGetProducts.body);
     listOfProduct = listOfProduct.data.products;
+    
+    // CHECK If order can be fulfiled
+    const orderToBeFulfilled = productCheckIfEnoughStock(dataMain.orderProducts, listOfProduct)
 
-    // ORDER ITEMS QUANTITY <= STOCK ITEMS QUANTITY
-    dataMain.orderProducts.forEach(orderItem => {
-      for (let i = 0; i < listOfProduct.length; i++) {
-        let stockItem = listOfProduct[i];
-        if(stockItem.title.toLowerCase() === orderItem.title.toLowerCase() && orderItem.quantity <= stockItem.stock){
-          orderToBeFulfilledCount += 1;
-          break;
-        }
-      }
-    })
-    if(orderToBeFulfilledCount === dataMain.orderProducts.length){
-      orderToBeFulfilled = true;
-    }
     if(orderToBeFulfilled){
 
-      // CHECK IF ORDER EXISTS
-      request(reqOptCheckExtOrderExists, (error, resCheckExtOrderExists, body) => {
-        let bool;
-        if(body.includes("order")){
-          bool = true
-        } else {
-          bool = false;
-        }
-        if(!bool) {
-          // ADDING ORDER
-          request(reqOptAddOrder, (error, resAddOrder, body) => {
+      // CHECK if order exists
+      request(orderCheckExtExistsReqOptsRes, (error, resCheckExtOrderExists, body) => {
+        if(resCheckExtOrderExists.statusCode === 204) {
+
+          // ADDING order
+          request(orderAddReqOptsRes, (error, resAddOrder, body) => {
             dataMain.orderID = resAddOrder.body.data.order.id;
-            //ITERATE OVER ORDER ITEMS
+
+            // ITERATE order items
             for (let i = 0; i < dataMain.orderProducts.length; i++) {
               const orderItem = dataMain.orderProducts[i];
-              // ITERATE OVER STOCK ITEMS
+
+              // ITERATE stock items
               for (let i = 0; i < listOfProduct.length; i++) {
                 const stockItem = listOfProduct[i];
-                // UPDATE STOCK = STOCK - ORDER
-                reqOptUpdateStock.json.id = stockItem.id
-                reqOptUpdateStock.json.stock = stockItem.stock - orderItem.quantity;
+
+                // UPDATE stock
+                productUpdateStockReqOptsRes.json.id = stockItem.id
+                productUpdateStockReqOptsRes.json.stock = stockItem.stock - orderItem.quantity;
                 if(orderItem.title.toLowerCase() === stockItem.title.toLowerCase()){
-                  request(reqOptUpdateStock, (error, resUpdateStock, body) => {
+                  request(productUpdateStockReqOptsRes, (error, resUpdateStock, body) => {
                     console.log("Updating stock")
                   })
                 }
@@ -268,4 +150,61 @@ let CheckOrderFulfilment = async (reqOptCheckExtOrderExists, reqOptAddOrder, req
   })
 }
 
-module.exports = router;
+// ORDER UPDATE
+router.put('/api/v1/klf/shopify/:supplierID/:orderID', async (req, res) => {
+  try {
+    // Variable declarations
+    const listOfOrderProducts = productFormatOrderProducts(req.body.data.line_items);
+    let dataMain = klfDataMainDecleration(req.body.data, listOfOrderProducts, req.params.supplierID, "update", req.params.orderID);
+
+    const orderUpdateReqOptsRes = orderUpdateReqOpts(dataMain);
+    const productUpdateStockReqOptsRes = productUpdateStockReqOpts(dataMain);
+
+    CheckOrderFulfilmentUpdate(orderUpdateReqOptsRes, productUpdateStockReqOptsRes, dataMain, res);
+
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+let CheckOrderFulfilmentUpdate = async (orderUpdateReqOptsRes, productUpdateStockReqOptsRes, dataMain, res) =>  {
+  request(productGetReqOptsRes, (error, resGetProducts, body) => {
+    let listOfProduct = JSON.parse(body);
+    listOfProduct = listOfProduct.data.products;
+    // CHECK If order can be fulfiled
+    const orderToBeFulfilled = productCheckIfEnoughStock(dataMain.orderProducts, listOfProduct)
+    if(orderToBeFulfilled){
+      // UPDATING ORDER
+      request(orderUpdateReqOptsRes, (error, resUpdateOrder, body) => {
+        dataMain.orderID = resUpdateOrder.body.data.order.id;
+        //ITERATE OVER ORDER ITEMS
+        for (let i = 0; i < dataMain.orderProducts.length; i++) {
+          const orderItem = dataMain.orderProducts[i];
+          // ITERATE OVER STOCK ITEMS
+          for (let i = 0; i < listOfProduct.length; i++) {
+            const stockItem = listOfProduct[i];
+            // UPDATE STOCK = STOCK - ORDER
+            productUpdateStockReqOptsRes.json.id = stockItem.id
+            productUpdateStockReqOptsRes.json.stock = stockItem.stock - orderItem.quantity;
+            if(orderItem.title.toLowerCase() === stockItem.title.toLowerCase()){
+              console.log("Updating stock")
+              request(productUpdateStockReqOptsRes, (error, resUpdateStock, body) => {
+              })
+            }
+          }
+        }
+        email.SendEmailToKaientaiOrderUpdate(dataMain);
+        res.status(200).json({
+          status: "OK."
+        })
+      })
+    } else {
+      res.status(406).json({
+        status: "Order cannot be fulfiled."
+      })
+      console.log("Order cannot be fulfiled.")
+    }
+  })
+}
+
+export default router;
