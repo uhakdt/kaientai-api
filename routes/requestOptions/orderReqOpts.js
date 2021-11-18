a47dca03d77a1b751c4976de68c58284dcdb5017
uhakdt <uhakdt@gmail.com>
2021-11-18 22:12:49 +0000

    Order Update ReqOpts

--- routes/requestOptions/orderReqOpts.js
+++ routes/requestOptions/orderReqOpts.js
@@ -34,3 +34,31 @@ export function orderAddReqOpts (dataMain) {
   };
   return res;
 }
+
+export function orderUpdateReqOpts (dataMain) {
+  const res = {
+    url: `${currentURL}/api/v1/order`,
+    method: 'PUT',
+    json: {
+      "id": dataMain.orderID,
+      "dateAndTime": dataMain.dateAndTime,
+      "statusID": 1,
+      "supplierID": dataMain.supplierID,
+      "userID": null,
+      "totalAmount": dataMain.totalAmount,
+      "contactName": dataMain.name,
+      "contactEmail": dataMain.email,
+      "contactPhone": dataMain.phone,
+      "address1": dataMain.address1,
+      "address2": dataMain.address2,
+      "city": dataMain.city,
+      "county": dataMain.county,
+      "country": dataMain.country,
+      "postcode": dataMain.postcode,
+      "offerID": null,
+      "extOrderID": dataMain.extOrderID,
+      "cartProducts": dataMain.orderProducts
+    },
+  };
+  return res;
+}
