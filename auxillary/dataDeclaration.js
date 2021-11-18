4273ebf036bcd401cfdfaabc02d75458f74e9ba7
uhakdt <uhakdt@gmail.com>
2021-11-18 22:11:30 +0000

    Now dataDeclaration allows for the updateOrder as well.

--- auxillary/dataDeclaration.js
+++ auxillary/dataDeclaration.js
@@ -1,8 +1,8 @@
 import { GetDateAndTimeNow } from './dateAndTimeNow';
 
-export function klfDataMainDecleration (dataBody, listOfOrderProducts, supplierID) {
+export function klfDataMainDecleration (dataBody, listOfOrderProducts, supplierID, type, orderID) {
   let res = {
-    orderID: null,
+    orderID: type === "add" ? null : orderID,
     name: dataBody.customer.first_name + " " + dataBody.customer.last_name,
     email: dataBody.customer.email,
     phone: dataBody.customer.phone,
