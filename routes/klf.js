dba08ff6d1fdb2c535a6774404b22344edf4e19c
uhakdt <60895762+uhakdt@users.noreply.github.com>
2021-12-08 14:50:46 +0000

    ignore.

--- routes/klf.js
+++ routes/klf.js
@@ -25,9 +25,6 @@ router.post('/api/v1/klf/:platform/:supplierID', async (req, res) => {
       listOfOrderProducts = productFormatOrderProductsShopify(req.body.data.line_items);
       dataMain = klfDataMainDeclerationShopify(req.body.data, listOfOrderProducts, req.params.supplierID, "add", null, "In Progress");
     } else if(req.params.platform === "woocommerce") {
-      console.log("HELOOOOOOOO")
-      console.log(req.body.data)
-      console.log("HELOOOOOOOO")
       listOfOrderProducts = productFormatOrderProductsWooCommerce(req.body.data.line_items);
       dataMain = klfDataMainDeclerationWooCommerce(req.body.data, listOfOrderProducts, req.params.supplierID, "add", null, "In Progress");
     }
